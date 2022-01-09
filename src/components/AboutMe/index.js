55db69291d5a617852b8450921646dc01f8ceda2
EdyS12 <eduardajsilva20@gmail.com>
2022-01-09 22:08:41 +0000

    texto html

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -9,19 +9,22 @@ const AboutMe = () => {
         <TextContainer>
           <Titulo>ABOUT ME</Titulo>
           <Texto>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Augue ut
-            eget augue fringilla enim nisl, mauris, convallis. Mollis
-            ullamcorper id tristique tortor id pharetra. Ut risus lacus
-            venenatis iaculis euismod. Mauris amet maecenas enim hac fermentum
-            et amet, penatibus. Lacus, neque, id leo id elementum nunc tellus
-            justo, lorem. Ac facilisi nulla nulla ipsum mauris, purus. Viverra
-            ullamcorper neque facilisi sed dolor ullamcorper donec auctor.
-            Tincidunt tellus feugiat pulvinar elementum. Libero, nibh blandit
-            sed eleifend sollicitudin magna posuere sit sed. Morbi vestibulum,
-            etiam lectus amet, diam aliquam. Massa, etiam lectus gravida eros.
-            Amet, tincidunt quis quam pharetra, venenatis dictum viverra cras
-            tortor. Morbi augue tempor bibendum tincidunt eleifend tortor. Orci
-            facilisis elit quis nunc sagittis et ut.
+            My name is Eduarda Silva, I'm 21 years old and I love everything
+            related to arts. I've recently completed my Bachelor's degree in
+            Design and Multimedia and I am currently studying to get my Master's
+            degree. <br></br> In my third year of college I joined Jeknowledge,
+            a Junior Enterprise, which not only allowed me to improve my
+            designer skills, but also helped me having a better understanding
+            about what it is like to work with real clients. In my years of
+            college I've developed several projects with diferent languages such
+            as Processing 3D, HTML, CSS and most recently React.js. <br></br>I'm
+            not going to lie, I still don't have a specific area where I want to
+            work on, but the thing about me is that I take ANY challenge from
+            editorial to web development and always try my best to do my best
+            work. <br></br>In my free time (I mean when I have free time) I like
+            to expirement with photography and illustration to expand my
+            artistic abilities. Go to "Projects" if you want to see what I am
+            capable of. Hope you enjoy it!
           </Texto>
         </TextContainer>
       </Container>
